47d63dfd297b47f89e112440c9eb2125e0e31951
Víťa Tauer <vita@nothrow.cz>
2016-10-09 20:44:57 +0200

    Created the project

---
+++ src/clacks.overhead/ClacksExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+
+namespace clacks.overhead
+{
+    /// <summary>
+    ///     Extension method for adding the <see cref="T:clacks.overhead.ClacksMiddleware" /> middleware to an
+    ///     <see cref="T:Microsoft.AspNetCore.Builder.IApplicationBuilder" />.
+    /// </summary>
+    public static class ClacksExtensions
+    {
+        /// <summary>
+        ///     Adds a <see cref="T:clacks.overhead.ClacksMiddleware" /> middleware to the specified
+        ///     <see cref="T:Microsoft.AspNetCore.Builder.IApplicationBuilder" />.
+        /// </summary>
+        /// <param name="builder">The <see cref="T:Microsoft.AspNetCore.Builder.IApplicationBuilder" /> to add the middleware to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder RememberTerryPratchett([NotNull] this IApplicationBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            return builder.UseMiddleware<ClacksMiddleware>();
+        }
+    }
+}
---
+++ src/clacks.overhead/ClacksMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace clacks.overhead
+{
+    /// <summary>
+    ///     Man is not dead while his name is still spoken
+    /// </summary>
+    public class ClacksMiddleware
+    {
+        private const string ClacksOverheadHttpHeader = "X-Clacks-Overhead";
+        private static readonly StringValues SirTerryPratchett = new StringValues("GNU Terry Pratchett");
+
+        private readonly RequestDelegate _next;
+
+        public ClacksMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        ///     Adds the X-Clacks-Overhead http parameter to the response
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                httpContext.Response.Headers.Add(ClacksOverheadHttpHeader, SirTerryPratchett);
+            }
+            catch (ArgumentException)
+            {
+                // Someone already added X-Clacks-Overhead. Deal with it gracefully
+            }
+            catch (NotSupportedException)
+            {
+                // We are not allowed to modify httpContext.
+            }
+
+            return _next.Invoke(httpContext);
+        }
+    }
+}
